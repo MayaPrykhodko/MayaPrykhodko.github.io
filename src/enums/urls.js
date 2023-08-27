d19762db7747fc2e3762a1a00641b9120f3ce410
MayaPrykhodko <mp13013@ukr.net>
2023-08-27 13:55:36 +0300

    updates api

--- src/enums/urls.js
+++ src/enums/urls.js
@@ -1,7 +1,7 @@
 const urls = {
-    kyivUrl: "http://api.openweathermap.org/data/2.5/weather?id=703448&appid=83388bf1586d1c2ce407b1f5c7ae14af",
-    londonUrl: "http://api.openweathermap.org/data/2.5/weather?id=2643743&appid=83388bf1586d1c2ce407b1f5c7ae14af",
-    newYorkUrl: "http://api.openweathermap.org/data/2.5/weather?id=5128638&appid=83388bf1586d1c2ce407b1f5c7ae14af"
+    kyivUrl: "https://api.openweathermap.org/data/2.5/weather?id=703448&appid=83388bf1586d1c2ce407b1f5c7ae14af",
+    londonUrl: "https://api.openweathermap.org/data/2.5/weather?id=2643743&appid=83388bf1586d1c2ce407b1f5c7ae14af",
+    newYorkUrl: "https://api.openweathermap.org/data/2.5/weather?id=5128638&appid=83388bf1586d1c2ce407b1f5c7ae14af"
 }
 
 export default urls;
